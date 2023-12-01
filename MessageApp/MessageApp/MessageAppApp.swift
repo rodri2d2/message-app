ccc2bbd7fafcca33a0cfc71e03c70662291cf132
Rodrigo Candido <rodrigoacandido@gmail.com>
2023-12-01 06:08:46 +0100

    Initial Commit - setup empty project

---
+++ MessageApp/MessageApp/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  MessageApp
+//
+//  Created by Rodrigo  Candido on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
